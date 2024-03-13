10dc876da05a27e6608576f25ee7f430dce3f219
mistydelacruz <misty.dela.cruz1@gmail.com>
2024-03-12 14:48:05 -1000

    Finished the button styling and layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /***************************************
 * Created by: Misty Dela Cruz
 * Created on: 2/7/2024
-* Last Modified: 2/7/2024
+* Last Modified: 3/12/2024
 * Updates:
 * Added the Button component and coding out the layout of the page
 ***************************************/
@@ -18,8 +18,15 @@ function App() {
         <div className='left-content'>        {/*Divider for the contents of the left side*/}
           <h1><strong>Welcome!</strong></h1>
           <p><strong>Please <strong>login</strong> to your account</strong></p>
-          <Button appearance='primary'>Hello World</Button>
-          <Button className='submit'>Submit</Button>
+          <div className='register'>                                                        {/*Divider for register*/}
+            <p>Don't have an account? <em>Register</em></p>
+          </div>
+          <div className='first-button'>                                                    {/*Divider for the first button login*/}
+            <Button appearance='primary'shouldFitContainer>Login with CAC</Button>
+          </div>
+          <div className='sec-button'>                                                      {/*Divider for the second button login*/}
+            <Button appearance='primary' shouldFitContainer>Login with YubiKey</Button>
+          </div>
         </div>
       </div>
     </>
